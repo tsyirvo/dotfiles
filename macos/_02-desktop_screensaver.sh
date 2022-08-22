4b81fb2ad9e04bd8d3fd90456293d5717343902a
tsyirvo <gabriel.hofman.00@gmail.com>
2022-08-22 18:16:24 +0200

    feat: redo the whole macos defaults

---
+++ macos/_02-desktop_screensaver.sh
@@ -0,0 +1,6 @@
+################################################################################
+# Desktop & Screen Saver                                                       #
+################################################################################
+
+# # Start after 30 Minutes
+defaults -currentHost write com.apple.screensaver idleTime -int 1800
