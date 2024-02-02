47c057460cd20da03bc3dbaeb07a35594bdab3b9
tsyirvo <gabriel.hofman.00@gmail.com>
2024-02-02 17:00:25 +0100

    feat: tweak git config

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,6 +1,6 @@
 ### Git ###
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
+alias gd='git difftool'
 alias ga='git add -A'
 alias gac='git add -A && git commit -m'
 alias gs='git status -sb'
@@ -8,6 +8,7 @@ alias gs='git status -sb'
 alias gcp="git checkout -"
 alias gcd="git checkout develop"
 alias gcm="git checkout master"
+alias gcb='git checkout -b $1'
 
 alias grc="git reset --soft HEAD~1"
 alias grs="git reset --soft HEAD ."
@@ -15,15 +16,9 @@ alias gsc='git stash && git stash clear'
 
 alias gp='git push origin HEAD'
 alias gpf='git push origin HEAD -f --force-with-lease'
-alias gpd='git pull origin develop'
-alias gpdr='git pull origin develop --rebase'
-alias gpm='git pull origin master'
-alias gpmr='git pull origin master --rebase'
 alias gpt='git push --follow-tags'
 alias gft='git fetch --tags'
 
-alias gco='git checkout origin HEAD'
-alias gcb='git checkout -b $1'
 alias gb='git branch'
 alias gt='git tag'
 
