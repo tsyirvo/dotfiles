58d9a7d8d2638dc3384886a858836884ac3bd92c
tsyirvo <gabriel.hofman.00@gmail.com>
2026-01-06 15:25:10 +0100

    feat: misc conf update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -78,3 +78,9 @@ fzf --fish | source
 # Set up atuin
 atuin init fish | source
 
+# amp
+set --export PATH "~/.local/bin" $PATH
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
